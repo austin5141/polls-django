71c0866bc8a5b3921c4f501c9da1011458a11eb6
austin5141 <168029784+austin5141@users.noreply.github.com>
2024-09-16 16:10:58 -0400

    Add files via upload

---
+++ django-polls/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Question, Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
